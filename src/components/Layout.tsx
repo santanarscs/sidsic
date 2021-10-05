51c10651ea1d3ce6069f45f29fc8f24ceb61b9e5
Raphael Santana <raphaelstn@gmail.com>
2021-10-05 16:16:06 -0300

    add nprogress and effects

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { ReactNode } from 'react'
 import { Sidebar } from './Sidebar'
-
+import { motion } from "framer-motion";
 type Props = {
   title?: string;
   children: ReactNode
@@ -9,16 +9,16 @@ type Props = {
 
 function Layout({title, children}: Props){
   return (
-    <div className="flex bg-gray-200 min-h-screen ">
+    <div className="flex bg-gray-200 min-h-screen "  >
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
       </Head>
       <Sidebar />
-      <div className="flex-1 flex flex-col p-4 mt-10">
+      <motion.div initial='initial' animate='animate' exit={{ opacity: 0 }} className="flex-1 flex flex-col p-4 mt-10">
         {children}
-      </div>
+      </motion.div>
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,41 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
 import '../styles/global.css'
 import 'leaflet/dist/leaflet.css';
+import NProgress from 'nprogress'
+import '../styles/nprogress.css'
+
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
   return (
-      <Component {...pageProps} />
+    <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
   )
 }
 
